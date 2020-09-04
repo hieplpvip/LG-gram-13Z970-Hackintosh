c2c5cfa766e6e1f927a57be4b21c05956c2a2511
Le Bao Hiep <baohiep2013@gmail.com>
2020-09-04 22:36:03 +0700

    WIP config

--- build.sh
+++ build.sh
@@ -5,12 +5,20 @@ SRCACPI='src/ACPI'
 
 rm -rf build && mkdir build
 
-$IASL build/SSDT-DMAC.aml $SRCACPI/SSDT-DMAC.dsl
-$IASL build/SSDT-EC-USBX.aml $SRCACPI/SSDT-EC-USBX.dsl
-$IASL build/SSDT-MEM2.aml $SRCACPI/SSDT-MEM2.dsl
-$IASL build/SSDT-PLUG.aml $SRCACPI/SSDT-PLUG.dsl
-$IASL build/SSDT-PMCR.aml $SRCACPI/SSDT-PMCR.dsl
-$IASL build/SSDT-PNLF.aml $SRCACPI/SSDT-PNLF.dsl
-$IASL build/SSDT-PPMC.aml $SRCACPI/SSDT-PPMC.dsl
-$IASL build/SSDT-SBUS-MCHC.aml $SRCACPI/SSDT-SBUS-MCHC.dsl
-$IASL build/SSDT-XOSI.aml $SRCACPI/SSDT-XOSI.dsl
+# Build ACPI
+mkdir build/ACPI
+$IASL build/ACPI/SSDT-DMAC.aml $SRCACPI/SSDT-DMAC.dsl
+$IASL build/ACPI/SSDT-EC-USBX.aml $SRCACPI/SSDT-EC-USBX.dsl
+$IASL build/ACPI/SSDT-MEM2.aml $SRCACPI/SSDT-MEM2.dsl
+$IASL build/ACPI/SSDT-PLUG.aml $SRCACPI/SSDT-PLUG.dsl
+$IASL build/ACPI/SSDT-PMCR.aml $SRCACPI/SSDT-PMCR.dsl
+$IASL build/ACPI/SSDT-PNLF.aml $SRCACPI/SSDT-PNLF.dsl
+$IASL build/ACPI/SSDT-PPMC.aml $SRCACPI/SSDT-PPMC.dsl
+$IASL build/ACPI/SSDT-SBUS-MCHC.aml $SRCACPI/SSDT-SBUS-MCHC.dsl
+$IASL build/ACPI/SSDT-XOSI.aml $SRCACPI/SSDT-XOSI.dsl
+
+# Copy UEFI Drivers
+cp -r src/Drivers build/Drivers
+
+# Copy OpenCore config
+cp src/config.plist build/config.plist
